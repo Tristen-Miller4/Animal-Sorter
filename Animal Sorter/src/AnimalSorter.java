7732d0839e2e466caa6639ccb1e18c2ee57b56c0
Summi <Summi@DESKTOP-6UC4OQQ>
2026-02-05 22:44:37 -0700

    Found the average age of the animals

--- Animal Sorter/src/AnimalSorter.java
+++ Animal Sorter/src/AnimalSorter.java
@@ -3,47 +3,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.io.IOException;
 import java.io.File;
+
 public class AnimalSorter
 
-	{
-		static String totalAge;
-		static int counter;
+{
+	static String totalAge;
+	static int counter;
 
-		public static void main(String[] args) throws IOException
-			{
-				Scanner animalSorter = new Scanner (new File ("AnimalSorter.txt"));
-				ArrayList<String> animalNames= new ArrayList <String>();
-				ArrayList<Integer> animalAges = new ArrayList <Integer>();
-				while (animalSorter.hasNextLine())
-					{
-					String word = animalSorter.nextLine();
-					//System.out.println(word);
-					String [] fredArray = word.split (",");
-					animalNames.add(fredArray[0]);
-					
+	public static void main(String[] args) throws IOException {
+		Scanner animalSorter = new Scanner(new File("AnimalSorter.txt"));
+		ArrayList<String> animalNames = new ArrayList<String>();
+		ArrayList<Integer> animalAges = new ArrayList<Integer>();
+		while (animalSorter.hasNextLine()) {
+			String word = animalSorter.nextLine();
+			// System.out.println(word);
+			String[] fredArray = word.split(",");
+			animalNames.add(fredArray[0]);
 
-					Collections.sort(animalNames);
-					
-					animalAges.add(Integer.parseInt(fredArray[1]));
-					totalAge += (Integer.parseInt(fredArray[1]));
-					counter++;
-					}
-				
-				
-				
-				
-				for (String a: animalNames)
-					{
-						System.out.println(a);
-					}
-					
-					
-				for (Integer b : animalAges)
-				{
-					System.out.println(b);
-				}
-					
+			Collections.sort(animalNames);
 
-			}
+			animalAges.add(Integer.parseInt(fredArray[1]));
+			totalAge += (Integer.parseInt(fredArray[1]));
+			counter++;
+		}
 
+		System.out.println("List of Alphabetized animal Names");
+		System.out.println();
+		for (String a : animalNames) {
+			System.out.println(a);
+		}
+
+		
 	}
+	
+}
